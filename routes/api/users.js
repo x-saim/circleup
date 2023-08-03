ce94b33970a76edda8d21d4bdd6379997d5f60de
Saim Amir <saimxamir@gmail.com>
2023-08-02 23:50:21 -0400

    set up pseudocode for logic to handle user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router(); // Create an instance of Express router
 const {check, validationResults} = require('express-validator');
 
+const User = require('models/User.js')
+
 // @route   POST api/users
 // @desc    Register User
 // @access  Public
@@ -31,7 +33,35 @@ router.post('/',
       // If there are validation errors, send a 400 response with the errors
       return res.status(400).json({errors: errors.array()});
     }
+
     // If validation passes, proceed with the route handler logic
+    
+    const {name, email, password} = req.body;
+    
+    try {
+    //see if user exists
+
+
+    //get users gravatar
+
+
+    // encrypt password
+
+
+    //return jsonwebtoken
+    } catch (error) {
+      
+    }
+
+
+
+
+
+   
+    
+    
+    
+    
     // Send a response back to the client
     res.send('User route');
 
