02073ae8d026b41a29e6f90c85147ac08f8d89f5
Saim Amir <saimxamir@gmail.com>
2023-08-02 23:46:13 -0400

    add comments to post req

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,28 +1,38 @@
 //Handle registering users, adding users.
 
 const express = require('express');
-const router = express.Router();
-const {check, vaalidationResults, validationResult} = require('express-validator');
+const router = express.Router(); // Create an instance of Express router
+const {check, validationResults} = require('express-validator');
 
 // @route   POST api/users
 // @desc    Register User
 // @access  Public
 
 router.post('/',
-  [check('name', 'Name is required')
-    .not()
-    .isEmpty(),
-  check('email','Please include a valid email')
-    .isEmail(),
-  check('password','Please enter a password with 6 or more characters')
-    .isLength({min:6})
+  [
+    // Validation middleware for 'name' field
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+
+    // Validation middleware for 'email' field
+    check('email','Please include a valid email')
+      .isEmail(),
+
+    // Validation middleware for 'password' field
+    check('password','Please enter a password with 6 or more characters')
+      .isLength({min:6})
   ],
   (req, res) => {
 
-    const errors = validationResult(req);
+    // Extract validation errors, if any
+    const errors = validationResults(req);
     if (!errors.isEmpty()) {
+      // If there are validation errors, send a 400 response with the errors
       return res.status(400).json({errors: errors.array()});
     }
+    // If validation passes, proceed with the route handler logic
+    // Send a response back to the client
     res.send('User route');
 
 
