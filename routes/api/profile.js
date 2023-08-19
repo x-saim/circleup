c59508c52d4fc396ec57227eddd8009e299481cb
Saim Amir <saimxamir@gmail.com>
2023-08-18 23:34:12 -0400

    create a profile fields obj with properties

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -42,8 +42,50 @@ async(req, res) => {
     return res.status(400).json({errors: errors.array()});
   }
 
+  const {
+    company,
+    website,
+    location,
+    bio,
+    status,
+    githubusername,
+    skills,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedin
+  } = req.body;
 
-});
+  //Build Profile Obj
+
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (company) profileFields.company = company;
+  if (website) profileFields.website = website;
+  if (location) profileFields.location = location;
+  if (bio) profileFields.bio = bio;
+  if (status) profileFields.status = status;
+  if (githubusername) profileFields.githubusername = githubusername;
+
+  // Skills - split into an array
+  if (skills) {
+    profileFields.skills = skills.split(',').map(skill => skill.trim());
+  }
+
+  // Social links
+  profileFields.social = {};
+  if (youtube) profileFields.social.youtube = youtube;
+  if (facebook) profileFields.social.facebook = facebook;
+  if (twitter) profileFields.social.twitter = twitter;
+  if (instagram) profileFields.social.instagram = instagram;
+  if (linkedin) profileFields.social.linkedin = linkedin;
+
+
+  
+}
+
+);
 
 
 module.exports = router;
