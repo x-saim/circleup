bf783ec7dcaf021d54ac4f53502464da9fac1431
Saim Amir <saimxamir@gmail.com>
2023-08-19 17:51:14 -0400

    set up GET route to get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -110,4 +110,22 @@ async(req, res) => {
 );
 
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+
+
+router.get('/', async(req, res) => {
+
+  try {
+    const profiles = await Profile.find().populate('user', ['name','avatar']);
+    res.json(profiles);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+
+})
+
 module.exports = router;
