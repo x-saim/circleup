8b01c95d2296d4ed860366b13d6bca2130368595
Saim Amir <saimxamir@gmail.com>
2023-08-18 21:29:04 -0400

    add middleware func to auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth')
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
 
-router.get('/', (req, res) => res.send('Auth route'));
+//adding auth as second parameter will run the middleware
+router.get('/', auth, (req, res) => res.send('Auth route'));
 
 module.exports = router;
