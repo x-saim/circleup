d0b64a989293cfc00ff87272c360a94465ea9380
Saim Amir <saimxamir@gmail.com>
2023-08-26 15:25:39 -0400

    fix route error and add POST route for adding posts

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connectDB();
 
 
 //Initializing Middleware
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 
 app.get('/', (req, res) => res.send('API Running'));
@@ -17,7 +17,7 @@ app.get('/', (req, res) => res.send('API Running'));
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/post', require('./routes/api/post'));
+app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
