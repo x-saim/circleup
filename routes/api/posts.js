aea9fb181355c6d1320cf9ee36f23b9b8d45b53f
Saim Amir <saimxamir@gmail.com>
2023-08-26 20:56:07 -0400

    add PUT req to unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -170,4 +170,42 @@ router.put('/like/:id', auth, async (req, res) => {
 
 })
 
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+
+  try {
+
+    const post = await Post.findById(req.params.id)
+
+    //Check if post has been unliked already.
+    //Checking if current user unliking is equal to the logged in user.
+    const checkLike = post.likes.filter(like => like.user.toString() === req.user.id).length === 0;
+
+    if (checkLike) {
+      return res.status(400).json({ msg: 'Post has not been liked.' })
+    }
+
+    //Delete user from likes array.
+
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error.');
+
+  }
+
+})
+
 module.exports = router;
