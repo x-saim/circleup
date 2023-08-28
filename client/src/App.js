25ce3f00c317d6fc5d86435a9de7febbe72d7900
Saim Amir <saimxamir@gmail.com>
2023-08-28 16:17:15 -0400

    add routes for login and register to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
+import { Login } from './components/auth/Login';
+import { Register } from './components/auth/Register';
+
 
 const App = () => (
   <Router>
@@ -10,11 +13,18 @@ const App = () => (
       <Navbar />
 
       <Routes>
-        <Route exact path='/' element={<Landing />} />
+        <Route path='/' element={<Landing />} />
       </Routes>
 
+      <section className='container'>
+        <Routes>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </section>
+
     </Fragment>
-  </Router>
+  </Router >
 );
 
 export default App;
